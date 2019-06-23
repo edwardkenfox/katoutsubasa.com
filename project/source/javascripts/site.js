cfd817b25096223b1824160de868506d5ed1bea2
Edward Fox <edwardkenfox@gmail.com>
2019-06-23 15:03:45 +0900

    Dynamically add active style to menu links

--- project/source/javascripts/site.js
+++ project/source/javascripts/site.js
@@ -1,1 +1,15 @@
-// This is where it all goes :)
+makeLinkActive = () => {
+  const currentPath = location.pathname.split('/')[1]
+  if (currentPath === '') {
+    return false;
+  }
+  const menuLinks = document.querySelectorAll('.nav-link');
+  [].forEach.call(menuLinks, el => {
+    if (el && el.innerText.toLowerCase() === currentPath) {
+      el.classList.add('active')
+      return
+    }
+  })
+
+}
+makeLinkActive();
